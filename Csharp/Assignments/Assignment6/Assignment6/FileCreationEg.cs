d233cd2fcc90bb4ad026be1a903c360b188f29c6
JAHNAVI SAKALA <jahnavis@infinite.com>
2025-07-06 22:22:08 +0530

    Assignment 6 submitted

---
+++ Csharp/Assignments/Assignment6/Assignment6/FileCreationEg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment6
+{
+    class FileCreationEg
+    {
+        //second question
+        // Write a program in C# Sharp to create a file and write an array of strings to the file.
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+            Console.WriteLine("Enter lines of text (press enter on an empty line to finish):");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                //stop
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+                lines.Add(input);
+            }
+            Console.WriteLine("Enter the filename:");
+            string filename = Console.ReadLine();
+            try
+            {
+                File.WriteAllLines(filename, lines);
+                Console.WriteLine("File created successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured: " + ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Csharp/Assignments/Assignment6/Assignment6/FileLinesCountEg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment6
+{
+    //Third Question
+    // Write a program in C# Sharp to count the number of lines in a file.
+    class FileLinesCountEg
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the file name to read: ");
+            string filename = Console.ReadLine();
+            try
+            {
+                string[] lines = File.ReadAllLines(filename);
+                int linecount = lines.Length;
+                Console.WriteLine($"The file {filename} has {linecount} lines.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The specified file was not found");
+            }
+            Console.Read();
+        }
+    }
+}
